73cfd2c1293243e3a45f027f4c05c1723c9fc47b
Kwabena Boakye <kwaboakye93@gmail.com>
2024-04-08 14:29:50 +0000

    Update navigation menu toggle functionality in script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 
 const navToggler = document.querySelector(".nav-toggler");
 const navMenu = document.querySelector("nav ul");
-const togglerIcon = document.querySelector(".nav-toggler img");
+const openIcon = document.querySelector(".nav-toggler #openIcon");
+const closeIcon = document.querySelector(".nav-toggler #closeIcon");
 
 navToggler.addEventListener("click", () => {
-    console.log(togglerIcon.src);
     if (navMenu.classList.contains("translate-x-0")) {
 
         navMenu.classList.add("translate-x-full");
         navMenu.classList.remove("translate-x-0");
-        togglerIcon.src = "/assets/images/icon-menu.svg";
+        openIcon.style.display = "block";
+        closeIcon.style.display = "none";
     } else {
         navMenu.classList.remove("translate-x-full");
         navMenu.classList.add("translate-x-0");
-        togglerIcon.src = "/assets/images/icon-menu-close.svg";
+        closeIcon.style.display = "block";
+        openIcon.style.display = "none";
     }
 });
